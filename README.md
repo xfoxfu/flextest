522be9f3a5af548c9b8deaef2da9c55ddc65e1c6
Yuze Fu <i@xfox.me>
2020-09-27 17:04:27 +0800

    feat: initial poc

--- README.md
+++ README.md
@@ -1,10 +1,89 @@
 # flextest
 
-A flexible test runner for JavaScript. Targeted at fundamental building parts of a test framework.
+A flexible test runner for JavaScript, highly inspired by [React Hooks](https://reactjs.org/docs/hooks-intro.html) for state management. Targeted at fundamental building parts of a test framework.
 
 - tests are normal JavaScript files that can be run directly
 - no built-in assertion library, but have pluggable support for assertions
 - a modular design, which can be consumed by upper libraries
+- a Hook-like syntax for declaring tranditional `before` and `after`
+
+## Introduction
+
+The library only introduces three functions, with which the all essential parts of a test library can be achieved.
+
+### `test`
+
+Use `test` to declare a test case.
+
+```ts
+test("a + b", () => {
+  assert(1 + 2 === 3);
+});
+```
+
+Test cases can be nested.
+
+```ts
+test("a + b", () => {
+  assert(1 + 2 === 3);
+  test("NaN", () => {
+    assert(1 + NaN === NaN);
+  });
+  test("undefined", () => {
+    assert(1 + undefined === undefined);
+  });
+});
+```
+
+### `hook`
+
+Use hook to declare state to setup and teardown test environments. There are three main usages of `hook` function.
+
+First, `hook` can be used as a simple setup function with return value.
+
+```ts
+const value = hook(() => {
+  return 1;
+});
+// value === 1
+```
+
+As functions without returning value can be considered as returning `undefined`, `hook` in this way can also be used as a one-shot setup function.
+
+Second, if a function returning another function is passed to `hook`, then the returned function will be considered as a teardown function.
+
+```ts
+hook(() => {
+  console.log("setup");
+  return () => {
+    console.log("teardown");
+  };
+});
+/*
+ * This will output:
+ * setup
+ * teardown
+ */
+```
+
+The setup part will run immediately, while teardown part will run after the surrounding test case finished.
+
+Finally, the function may return `[value, teardown]` to combine the first part and the second part.
+
+```ts
+const value = hook(() => {
+  console.log("setup");
+  return [1, () => console.log("teardown")];
+});
+```
+
+### `finish`
+
+Waits for all async tests to complete, and generates report to the console. It can be directly called.
+
+```ts
+finish();
+```
 
 ## License
 
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,93 @@
-export const MODULE_VERSION = "1.0.0";
+const HOOK_STACK: (() => void)[][] = [];
+
+type IHook = {
+  (up: () => () => void): void;
+  <T>(up: () => T): T;
+  <T>(up: () => [T, () => void]): T;
+};
+export const hook: IHook = <T>(
+  up: () => T | (() => void) | [T, () => void],
+): T => {
+  if (typeof up !== "function") {
+    throw new TypeError("hook parameter must be a function");
+  }
+  const ret = up();
+  if (typeof ret === "function") {
+    // useHook(up: () => () => void): void;
+    HOOK_STACK[HOOK_STACK.length - 1].push(ret as () => void);
+    return (undefined as unknown) as T;
+  } else if (
+    Array.isArray(ret) &&
+    ret.length === 2 &&
+    typeof ret[ret.length - 1] === "function"
+  ) {
+    // function useHook<T>(up: () => [T, () => void]): T;
+    HOOK_STACK[HOOK_STACK.length - 1].push(ret[1]);
+    return ret[0];
+  }
+  return ret as T;
+};
+
+type TestResult = {
+  name: string;
+  status: "ok" | "failed" | "pending";
+  childs: TestResult[];
+};
+
+const TEST_ROOT: TestResult = {
+  name: "$root",
+  status: "ok",
+  childs: [],
+};
+let CURRENT_PARENT: TestResult | null = null;
+let CURRENT_NODE: TestResult = TEST_ROOT;
+
+export const test = (name: string, f: () => void): void => {
+  CURRENT_NODE.childs.push({
+    name,
+    status: "pending",
+    childs: [],
+  });
+
+  // traverse
+  const parentRecover = CURRENT_PARENT;
+  CURRENT_PARENT = CURRENT_NODE;
+  CURRENT_NODE = CURRENT_NODE.childs[CURRENT_NODE.childs.length - 1];
+  // setup
+  HOOK_STACK.push([]);
+
+  // run tests
+  let result: TestResult["status"] = "pending";
+  try {
+    f();
+    result = "ok";
+  } catch {
+    result = "failed";
+  }
+
+  // teardown
+  for (const fn of HOOK_STACK[HOOK_STACK.length - 1]) {
+    fn();
+  }
+  HOOK_STACK.pop();
+
+  CURRENT_NODE.status = result;
+  // recover
+  if (CURRENT_PARENT !== null) CURRENT_NODE = CURRENT_PARENT;
+  else CURRENT_NODE = TEST_ROOT;
+  CURRENT_PARENT = parentRecover;
+};
+
+export const finish = (node?: TestResult, prefix?: string): void => {
+  if (node === undefined) {
+    node = TEST_ROOT;
+  }
+  let curPrefix = "";
+  if (node !== TEST_ROOT) {
+    curPrefix = `${prefix ? `${prefix} > ` : ""}${node.name}`;
+    console.log(curPrefix, node.status);
+  }
+  for (const child of node?.childs) {
+    finish(child, curPrefix);
+  }
+};
